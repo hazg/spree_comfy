9689b5d2f3eb728e8879055f1c5e1657d11a144c
hazg <hazg@mail.ru>
2015-03-24 18:48:19 +0300

    icon fix

--- app/admin/tabs_decorator.rb
+++ app/admin/tabs_decorator.rb
@@ -2,6 +2,6 @@ Deface::Override.new(
                      :virtual_path => 'spree/admin/shared/_menu',
                      :name => 'custom-admin-tab',
                      :insert_bottom => "[data-hook=admin_tabs]",
-                     :text => "<%= tab(:pages, :url => '/cms_admin', :icon => 'icon-sitemap') %>"
+                     :text => "<%= tab(:pages, :url => '/cms_admin', :icon => 'icon-sitemap fa-sitemap') %>"
 )
 
