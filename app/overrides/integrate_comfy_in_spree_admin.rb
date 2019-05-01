8a3daf13cea841a68bfe6c6f45613cc767c983a8
Aleksandr Aleksandrov <hazg@mail.ru>
2019-05-01 17:58:12 +0300

    refactory

--- app/overrides/integrate_comfy_in_spree_admin.rb
+++ app/overrides/integrate_comfy_in_spree_admin.rb
@@ -3,13 +3,13 @@ Deface::Override.new(
   virtual_path: 'spree/shared/_head',
   name: 'integrate_head_title',
   replace: 'title',
-  partial: 'spree/admin/shared/head_title'
+  partial: 'spree/admin/shared/comfy_head_title'
 )
 
 # Left menu
 Deface::Override.new(
   virtual_path: 'spree/admin/shared/_main_menu',
-  name: 'comfy_admin_comfy_menu',
+  name: 'comfy_admin_spree_menu',
   insert_bottom: 'nav',
   partial: 'spree/admin/shared/comfy_submenu'
 )
@@ -17,14 +17,35 @@ Deface::Override.new(
 # Inside head
 Deface::Override.new(
   virtual_path: 'spree/layouts/admin',
-  name: 'comfy_admin_layout_head',
-  insert_bottom: '[data-hook="admin_inside_head"]',
+  name: 'comfy_admin_layout_spree_head',
+  insert_bottom: 'head',
   partial: 'spree/admin/shared/comfy_inside_head'
 )
 
+# Footer scripts
 Deface::Override.new(
   virtual_path: 'spree/layouts/admin',
-  name: 'comfy_admin_layout_head',
+  name: 'comfy_admin_layout_spree_footer',
   insert_bottom: '[data-hook="admin_footer_scripts"]',
   partial: 'spree/admin/shared/comfy_footer'
+)
+
+# Body id class
+Deface::Override.new(
+  virtual_path: 'spree/layouts/admin',
+  name: 'comfy_admin_body_id',
+  set_attributes: 'body',
+  attributes: {
+    id: "<%= (controller.class < Comfy::Admin::Cms::BaseController) ? 'comfy':'' %>"
+  }
+)
+
+# Main > row class
+Deface::Override.new(
+  virtual_path: 'spree/layouts/admin',
+  name: 'comfy_admin_main_class',
+  set_attributes: 'main > .row',
+  attributes: {
+    id: "<%= (controller.class < Comfy::Admin::Cms::BaseController) ? 'cms-main' : ''%>"
+  }
 )
